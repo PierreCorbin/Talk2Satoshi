408f4105a7a39df077de249c2e09614c6d181d89
Pierre <corbin.pierre92@gmail.com>
2023-05-10 15:18:50 +0200

    Added info text

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ from langchain.vectorstores import Pinecone
 def create_app_header():
     st.set_page_config(page_title="Talk2Satoshi", page_icon=":guardsman:")
     st.title("Talk2Satoshi")
-    st.write("Welcome, pleb! Here, you can chat with Satoshi Nakamoto.")
+    st.write("Welcome, pleb! Get answers to your bitcoin questions from the creator himself.")
+    st.divider()
+    st.write("The world of Bitcoin and cryptocurrency can be confusing and overwhelming, with new information and updates coming out every day.")
+    st.write("At Talk2Satoshi, we offer a unique way to get your questions answered - by chatting with an AI version of the creator of Bitcoin himself, Satoshi Nakamoto.")
+    st.write("Our AI is based on a big library of bitcoin books and all of the content written by Satoshi, making it the closest experience to chatting with Satoshi Nakamoto.")
+    st.divider()
     
 def display_widgets() -> str:
     query = st.text_input("Ask Satoshi a question:")
     if not query:
         st.error("Ask a question for this to work.")
+    
+    st.write('## Resources')
+    st.write("""Here is the list of all the data used by this model: \n
+    - The Price of Tomorrow, Jeff Booth, 2020 \n
+    """)
 
     return query
 
